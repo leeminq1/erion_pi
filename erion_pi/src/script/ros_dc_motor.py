944e65f80ab0d365b880a44d307f777b80300fb6
leeminq1 <min2195@naver.com>
2021-08-30 14:50:04 +0900

    app_control

--- erion_pi/src/script/ros_dc_motor.py
+++ erion_pi/src/script/ros_dc_motor.py
@@ -22,8 +22,8 @@ def get_firebase_input_value():
     doc_ref = db.collection(u'pi').document(u'key')
     doc = doc_ref.get()
     input_value = doc.to_dict()['input']
-    print("check the initial_value")
-    print(f'Document data: {input_value}')
+    # print("check the initial_value")
+    # print(f'Document data: {input_value}')
     return input_value
 
 
@@ -33,13 +33,21 @@ def callback(msg):
     cmd_vel.data[2] = int(msg.data[2])
 
     input_value = get_firebase_input_value()
-    print(input_value)
+    # print(input_value)
 
     # mode detection
     f_autodrvie = cmd_vel.data[2] == 0
     f_keyboard = cmd_vel.data[2] == 1
     f_app_control = cmd_vel.data[2] == 2
     f_lift_mode = cmd_vel.data[2] == 3
+    if f_autodrvie:
+        print("autodrive mode")
+    elif f_keyboard:
+        print("keyboard mode")
+    elif f_app_control:
+        print("appcontrol mode")
+    elif f_lift_mode:
+        print("lift mode")
 
     # drive deteciton
     f_forward = (cmd_vel.data[0] > 0) and (cmd_vel.data[1] == 0)
@@ -51,26 +59,26 @@ def callback(msg):
     f_vehicle_stop = (cmd_vel.data[0] == 0) and (cmd_vel.data[1] == 0)
 
     # keyboard control
-    if f_keyboard or (f_app_control and input_value == "go"):
-        if f_forward:
+    if f_keyboard or f_app_control:
+        if f_forward or input_value == "go":
             # rospy.loginfo("accel : {} \t angular: {}".format(
             #     msg.data[0], msg.data[1]))
             iMotor.start("forward")
             print("----keyboard forward-----")
 
-        elif f_backward or (f_app_control and input_value == "back"):
+        elif f_backward or input_value == "back":
             # rospy.loginfo("accel : {} \t angular: {}".format(
             #     msg.data[0], msg.data[1]))
             iMotor.start("backward")
             print("----keyboard backward-----")
 
-        elif f_forward_right or (f_app_control and input_value == "right"):
+        elif f_forward_right or input_value == "right":
             # rospy.loginfo("accel : {} \t angular: {}".format(
             #     msg.data[0], msg.data[1]))
             iMotor.start("forward_right")
             print("----keyboard forward - right-----")
 
-        elif f_forward_left or (f_app_control and input_value == "left"):
+        elif f_forward_left or input_value == "left":
             # rospy.loginfo("accel : {} \t angular: {}".format(
             #     msg.data[0], msg.data[1]))
             iMotor.start("forward_left")
@@ -88,7 +96,7 @@ def callback(msg):
             iMotor.start("backward_left")
             print("----keyboard backward  left-----")
 
-        elif f_vehicle_stop or (f_app_control and input_value == "stop"):
+        elif f_vehicle_stop or input_value == "stop":
             # rospy.loginfo("accel : {} \t angular: {}".format(
             #     msg.data[0], msg.data[1]))
             print("--keyboard vehicle stop--")
